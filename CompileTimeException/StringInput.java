eb23ddf5851947cd69080bdf808a1b4956fc660e
Rakin Saad Aftab <66327542+SAAD-X@users.noreply.github.com>
2020-09-22 23:26:16 +0600

    Update StringInput.java

--- CompileTimeException/StringInput.java
+++ CompileTimeException/StringInput.java
@@ -4,7 +4,7 @@ import java.io.*;
 //readLine() is a method of BufferedReader
 //So, we need an object of BufferedReader class to call that method.
 //But, to create an object of BufferedReader, we need an object of InputStreamReader.
-//The parameterized constructor of InputStreamReader takes "System.in" as its parameter
+//The parameterized constructor of InputStreamReader takes "System.in" as its parameter.
 
 public class StringInput
 {
