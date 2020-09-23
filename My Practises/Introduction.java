8dc1eef45de03779f0909e775fbf9ee3a618c993
Rakin Saad Aftab <66327542+SAAD-X@users.noreply.github.com>
2020-09-23 12:00:32 +0600

    Update Introduction.java

--- My Practises/Introduction.java
+++ My Practises/Introduction.java
@@ -6,7 +6,6 @@ public class Introduction
 		System.out.println("UNIVERSITY NAME : AMERICAN INTERNATIONAL UNIVERSITY-BANGLADESH");
 		System.out.println("Name            : Rakin Sad Aftab");
 		System.out.println("ID              : 20-41991-1");
-		System.out.println("CGPA            : 3.45");
 		System.out.println("DEPATMENT       : CSE");
 	}
 }
